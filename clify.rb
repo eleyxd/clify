39d1b222cac7b13623f05a9e1e4befb5822b42d9
Vasiliy <marshellowdjvasilij@gmail.com>
2025-10-28 14:26:08 +0300

    Fixes: ffplay error
    fixed error when def main cant resolve "player" array

--- clify.rb
+++ clify.rb
@@ -1,6 +1,9 @@
 require 'shellwords'
 require 'open3'
-
+PLAYER = ['ffplay'].find { |p| system("which #{p} < /dev/null 2>&1")}
+PLAYER_ARGS = {
+  'ffplay' => '-nodisp -autoexit'
+}
 def get_url(track_url)
   puts "Waiting for stream: #{track_url}..."
   command = "yt-dlp -g --skip-download #{Shellwords.escape(track_url)}"
@@ -20,17 +23,15 @@ def get_url(track_url)
 end
 
 def play_stream(stream_url,player)
-  player = 'ffplay'
-  args = "-nodisp -autoexit"
   puts "Starting player (#{player})..."
-  puts "\n Press Q for end of streaming"
+  puts "\n Press Ctrl+C for end of streaming"
 
   args=PLAYER_ARGS[player]
   player_command = "#{player} #{args} #{Shellwords.escape(stream_url)}"
   system(player_command)
   puts "Streaming is completed"
 
-rescue Interrupt
+  rescue Interrupt
   puts "\n Streaming is interrupted by user"
 end
 
